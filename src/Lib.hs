b2ea84dff6eda153ea55c71a28488d885711bf92
Trip Volpe <trip.volpe@gmail.com>
2016-06-09 13:02:32 -0700

    Add comments and reduce indentation

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,20 +4,20 @@ module Lib
 
 bowlingScore :: [Int] -> Int
 bowlingScore = fst . foldl m (0, (10, 1, False, 1)) where
-    m (score, (pins, frame, second, mult)) roll =
-        (score', (pins', frame', second', mult')) where
-            score' = score + roll * mult
-            pins'
-                | rerack = 10
-                | otherwise = standing
-            frame'
-                | rerack = frame + 1
-                | otherwise = frame
-            second' = not rerack
-            mult'
-                | frame >= 10 = 1
-                | standing > 0 = max 1 $ mult - 1
-                | second = 2
-                | otherwise = 3
-            standing = pins - roll
-            rerack = standing == 0 || second
+  m (score, (pins, frame, second, mult)) roll =
+    (score', (pins', frame', second', mult')) where
+      score' = score + roll * mult
+      pins'
+        | rerack = 10
+        | otherwise = standing
+      frame'
+        | rerack = frame + 1
+        | otherwise = frame
+      second' = not rerack
+      mult'
+        | frame >= 10 = 1                   -- No mult after frame 10
+        | standing > 0 = max 1 $ mult - 1   -- Crap!
+        | second = 2                        -- Spare!
+        | otherwise = 3                     -- Strike!
+      standing = pins - roll
+      rerack = standing == 0 || second      -- Rerack on clear or second roll
