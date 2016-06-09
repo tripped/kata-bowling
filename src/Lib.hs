c695a4643eb13b4fc0abdf4ead5151cbd33eaa60
Trip Volpe <trip.volpe@gmail.com>
2016-06-09 11:52:54 -0700

    Add rerack binding

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,15 +8,16 @@ bowlingScore = fst . foldl m (0, (10, 1, False, 1)) where
         (score', (pins', frame', second', mult')) where
             score' = score + roll * mult
             pins'
-                | standing == 0 || second = 10
+                | rerack = 10
                 | otherwise = standing
             frame'
-                | standing == 0 || second = frame + 1
+                | rerack = frame + 1
                 | otherwise = frame
-            second' = not $ second || standing == 0
+            second' = not rerack
             mult'
                 | frame >= 10 = 1
                 | standing > 0 = max 1 $ mult - 1
                 | second = 2
                 | otherwise = 3
             standing = pins - roll
+            rerack = standing == 0 || second
