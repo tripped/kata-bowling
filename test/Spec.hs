59a201734dea38e12206d1549a5170614e4acb42
Trip Volpe <trip.volpe@gmail.com>
2016-06-09 13:41:18 -0700

    Add a failing test that exposes a bug in scoring

--- test/Spec.hs
+++ test/Spec.hs
@@ -26,6 +26,11 @@ main = hspec $ do
         it "tallies 299 for 11 strikes and one moment of regret" $ do
             bowlingScore [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9]
             `shouldBe` 299
+        it "correctly scores multiplier for second 10th frame roll" $ do
+            -- XXX: uh oh, spaghetti-os! This gets 290 somehow.
+            bowlingScore [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10]
+            --          ∑ 30  30  30  30  30  30  30  30  20  10  0  10 = 280
+            `shouldBe` 280
         it "tallies 190 for repeated 9/1 spare frames" $ do
             bowlingScore [ 9, 1
                          , 9, 1
