1b6d4d8417c825ddc1eecabf781247461b0f840e
Trip Volpe <trip.volpe@gmail.com>
2016-06-09 11:18:21 -0700

    Add initial bowlingScore implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
+    ( bowlingScore
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+--bowlingScore :: [Int] -> (Int, (Int, Int, Bool, Int))
+bowlingScore :: [Int] -> Int
+bowlingScore = fst . foldl m (0, (10, 1, False, 1)) where
+    m :: (Int, (Int, Int, Bool, Int)) -> Int -> (Int, (Int, Int, Bool, Int))
+    m (score, (pins, frame, second, mult)) roll =
+        (score', (pins', frame', second', mult')) where
+            score' = score + roll * mult
+            pins'
+                | standing == 0 || second = 10
+                | otherwise = standing
+            frame'
+                | standing == 0 || second = frame + 1
+                | otherwise = frame
+            second' = not $ second || standing == 0
+            mult'
+                | frame >= 10 = 1
+                | standing > 0 = max 1 $ mult - 1
+                | second = 2
+                | otherwise = 3
+            standing = pins - roll
