695f9a4130aead07928cf33d4663ba28af5dad7b
Trip Volpe <trip.volpe@gmail.com>
2016-06-09 13:31:47 -0700

    Pull fold function out of bowlingScore

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,22 +2,26 @@ module Lib
     ( bowlingScore
     ) where
 
+type GameState = (Int, (Int, Int, Bool, Int))
+
+scoreOne :: GameState -> Int -> GameState
+scoreOne (score, (pins, frame, second, mult)) roll =
+  (score', (pins', frame', second', mult')) where
+    score' = score + roll * mult
+    pins'
+      | rerack = 10
+      | otherwise = standing
+    frame'
+      | rerack = frame + 1
+      | otherwise = frame
+    second' = not rerack
+    mult'
+      | frame >= 10 = 1                   -- No mult after frame 10
+      | standing > 0 = max 1 $ mult - 1   -- Crap!
+      | second = 2                        -- Spare!
+      | otherwise = 3                     -- Strike!
+    standing = pins - roll
+    rerack = standing == 0 || second      -- Rerack on clear or second roll
+
 bowlingScore :: [Int] -> Int
-bowlingScore = fst . foldl m (0, (10, 1, False, 1)) where
-  m (score, (pins, frame, second, mult)) roll =
-    (score', (pins', frame', second', mult')) where
-      score' = score + roll * mult
-      pins'
-        | rerack = 10
-        | otherwise = standing
-      frame'
-        | rerack = frame + 1
-        | otherwise = frame
-      second' = not rerack
-      mult'
-        | frame >= 10 = 1                   -- No mult after frame 10
-        | standing > 0 = max 1 $ mult - 1   -- Crap!
-        | second = 2                        -- Spare!
-        | otherwise = 3                     -- Strike!
-      standing = pins - roll
-      rerack = standing == 0 || second      -- Rerack on clear or second roll
+bowlingScore = fst . foldl scoreOne (0, (10, 1, False, 1))
