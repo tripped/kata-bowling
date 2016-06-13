8824a8f1a67c175cafee96ec359bd00e43c04064
Trip Volpe <trip.volpe@gmail.com>
2016-06-13 15:28:42 -0700

    Refactor: simplify score patterns slightly

--- src/TDD.hs
+++ src/TDD.hs
@@ -8,8 +8,7 @@ bowl l = sum $ map (score.reverse) (inits l)
     where
         score :: [Int] -> Int
         score [] = 0
-        score [x] = x
-        score [x,y] = x
         score (x:y:z:xs)
             | y + z == 10 = 2*x
             | otherwise = x
+        score (x:xs) = x
