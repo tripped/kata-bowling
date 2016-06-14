59b8e65ec499fefb157908ee9f6c8415717bba83
Trip Volpe <trip.volpe@gmail.com>
2016-06-13 18:59:44 -0700

    Reformat: move some functions and add comments

--- src/TDD.hs
+++ src/TDD.hs
@@ -1,6 +1,5 @@
 module TDD
     ( bowl ) where
-
 import Data.List
 
 data Frame = Strike | Spare Int | Open Int Int
@@ -8,6 +7,16 @@ data Frame = Strike | Spare Int | Open Int Int
 
 bowl l = sum $ map score $ take 10 (windows.frames $ l)
     where 
+        -- Split a (valid) list of rolls into frames
+        frames (10:xs) = Strike : (frames xs)
+        frames [x] = [Open x 0]
+        frames (x:y:xs)
+            | x + y == 10 = Spare x : (frames xs)
+            | otherwise = Open x y : (frames xs)
+
+        -- Scoring is done on windows consisting of up to two following frames
+        windows all@(x:xs) = take 3 all : windows xs
+
         score (Strike : rs) = 10 + twoRolls rs
         score (Spare _ : rs) = 10 + oneRoll rs
         score (Open x y : _) = x + y
@@ -19,11 +28,3 @@ bowl l = sum $ map score $ take 10 (windows.frames $ l)
         oneRoll (Open x _ : _) = x
         oneRoll (Spare x : _) = x
         oneRoll (Strike : _) = 10
-
-        frames (10:xs) = Strike : (frames xs)
-        frames [x] = [Open x 0]
-        frames (x:y:xs)
-            | x + y == 10 = Spare x : (frames xs)
-            | otherwise = Open x y : (frames xs)
-
-        windows all@(x:xs) = take 3 all : windows xs
