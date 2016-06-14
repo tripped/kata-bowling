b58ab9a2258589c66d1ce5ed06db618080ef8195
Trip Volpe <trip.volpe@gmail.com>
2016-06-13 18:56:19 -0700

    Refactor: Move helper functions into bowl's where clause

--- src/TDD.hs
+++ src/TDD.hs
@@ -6,10 +6,6 @@ import Data.List
 data Frame = Strike | Spare Int | Open Int Int
     deriving Show
 
-windows :: [Frame] -> [[Frame]]
-windows [] = []
-windows all@(x:xs) = take 3 all : windows xs
-
 bowl l = sum $ map score $ take 10 (windows.frames $ l)
     where 
         score (Strike : rs) = 10 + twoRolls rs
@@ -26,10 +22,12 @@ bowl l = sum $ map score $ take 10 (windows.frames $ l)
         oneRoll (Spare x : _) = x
         oneRoll (Strike : _) = 10
 
-frames :: [Int] -> [Frame]
-frames [] = []
-frames (10:xs) = Strike : (frames xs)
-frames [x] = [Open x 0]
-frames (x:y:xs)
-    | x + y == 10 = Spare x : (frames xs)
-    | otherwise = Open x y : (frames xs)
+        frames [] = []
+        frames (10:xs) = Strike : (frames xs)
+        frames [x] = [Open x 0]
+        frames (x:y:xs)
+            | x + y == 10 = Spare x : (frames xs)
+            | otherwise = Open x y : (frames xs)
+
+        windows [] = []
+        windows all@(x:xs) = take 3 all : windows xs
