746fe23cea81e6d25f768df726a9b2435984d151
Trip Volpe <trip.volpe@gmail.com>
2016-06-15 21:50:27 -0700

    Refactor: extract frame->roll conversion to common rolls function

--- src/TDD.hs
+++ src/TDD.hs
@@ -19,10 +19,10 @@ bowl = sum . map score . take 10 . windows . frames
         score (Spare _ : rs) = 10 + oneRoll rs
         score (Open x y : _) = x + y
 
-        twoRolls (Open x y : _) = x + y
-        twoRolls (Spare _ : _) = 10
-        twoRolls (Strike : rs) = 10 + oneRoll rs
+        -- Reconstruct scorable rolls from a sequence of frames
+        rolls (Open x y : fs) = x : y : rolls fs
+        rolls (Spare x : fs) = x : 10 - x : rolls fs
+        rolls (Strike : fs) = 10 : rolls fs
 
-        oneRoll (Open x _ : _) = x
-        oneRoll (Spare x : _) = x
-        oneRoll (Strike : _) = 10
+        twoRolls = sum . take 2 . rolls
+        oneRoll = sum . take 1 . rolls
