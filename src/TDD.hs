c1b12849c642e47f59fd35d93f5a725649d589da
Trip Volpe <trip.volpe@gmail.com>
2016-06-15 21:59:17 -0700

    Refactor: write score in terms of rolls, delete oneRoll, twoRolls

--- src/TDD.hs
+++ src/TDD.hs
@@ -15,14 +15,11 @@ bowl = sum . map score . take 10 . windows . frames
         -- Scoring is done on windows consisting of up to two following frames
         windows all@(x:xs) = take 3 all : windows xs
 
-        score (Strike : rs) = 10 + twoRolls rs
-        score (Spare _ : rs) = 10 + oneRoll rs
+        score (Strike : after) = 10 + sum (take 2 $ rolls after)
+        score (Spare _ : after) = 10 + sum (take 1 $ rolls after)
         score (Open x y : _) = x + y
 
         -- Reconstruct scorable rolls from a sequence of frames
         rolls (Open x y : fs) = x : y : rolls fs
         rolls (Spare x : fs) = x : 10 - x : rolls fs
         rolls (Strike : fs) = 10 : rolls fs
-
-        twoRolls = sum . take 2 . rolls
-        oneRoll = sum . take 1 . rolls
