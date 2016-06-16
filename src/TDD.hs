221708aa3a83d0d98131997bbe12d5656ccba279
Trip Volpe <trip.volpe@gmail.com>
2016-06-15 21:34:56 -0700

    Reformat: remove some redundant parentheses

--- src/TDD.hs
+++ src/TDD.hs
@@ -6,11 +6,11 @@ data Frame = Strike | Spare Int | Open Int Int
 bowl = sum . map score . take 10 . windows . frames
     where 
         -- Split a (valid) list of rolls into frames
-        frames (10:xs) = Strike : (frames xs)
+        frames (10:xs) = Strike : frames xs
         frames [x] = [Open x 0]
         frames (x:y:xs)
-            | x + y == 10 = Spare x : (frames xs)
-            | otherwise = Open x y : (frames xs)
+            | x + y == 10 = Spare x : frames xs
+            | otherwise = Open x y : frames xs
 
         -- Scoring is done on windows consisting of up to two following frames
         windows all@(x:xs) = take 3 all : windows xs
